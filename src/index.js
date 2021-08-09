19e3f0a43c2d7765e67312ba94215ad7c0b1ccae
GHOST <ghostdevbusiness@gmail.com>
2021-08-09 22:06:58 +0100

    feat: check interval

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { Client, Intents } from 'discord.js';
 import { JellyCommands } from 'jellycommands';
+import { resolve } from 'path';
 
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
@@ -16,3 +17,36 @@ client.on('ready', () => {
 });
 
 client.login(process.env.TOKEN);
+
+import flatCache from 'flat-cache';
+
+const cache = flatCache.load('focus-interval', resolve('./focus-interval'));
+
+const add = (time, channel, guild) => {
+    const ending = Date.now() + time;
+
+    cache.setKey(Date.now(), { channel, ending, guild });
+    cache.save();
+};
+
+client.addFocus = add;
+
+setInterval(async () => {
+    const keys = Object.values(cache.all());
+
+    if (keys.length) {
+        for (const { channel: channelID, ending, guild: guildID } of keys) {
+            if (ending <= Date.now()) {
+                const guild = await client.guilds.fetch(guildID);
+                const channel = await guild.channels.fetch(channelID);
+
+                channel.permissionOverwrites.set([
+                    {
+                        id: guildID,
+                        allow: ['SPEAK'],
+                    },
+                ]);
+            }
+        }
+    }
+}, 5000);
